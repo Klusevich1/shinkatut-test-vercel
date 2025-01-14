30273d7a7ab328724bf32e1b04db6c3ba3e5b6fa
Egor <eklusevich@gmail.com>
2025-01-14 16:59:35 +0300

    add robots, sitemap, yandex verification and google

---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: 'https://shinkatut.by/',
+  generateRobotsTxt: true,
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="gLOwPvxqhx8hnPZ0WI0g8R-Klts_A_4tapFsKbl1mh8"
+        />
+        <meta name="yandex-verification" content="d43f05a6c816b873" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
